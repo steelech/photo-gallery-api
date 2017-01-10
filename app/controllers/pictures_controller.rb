6313237c6c12ebecd91a7870081d95f5961ea486
Charlie Steele <steelech@umich.edu>
2017-01-10 11:44:04 -0500

    Start file record creation on backend

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -9,7 +9,15 @@ class PicturesController < ApplicationController
 	end
 
 	def create
+		files = params[:files]
+		numFiles = params[:numFiles].to_i
+
 		render json: {}, status: 200
+		(0..numFiles).each do |file_numb|
+			puts "file: #{files[file_numb.to_s]}"
+			file = files[file_numb.to_s]
+			
+		end
 
 	end
 
